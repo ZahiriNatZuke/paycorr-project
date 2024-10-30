6e8780d62bf9be131ead5b766420e8b5dc36eaae
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2024-10-30 20:27:21 -0300

    feat: create safe image pipe

---
+++ src/app/core/pipes/safe-image.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform, SecurityContext, inject } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Multimedia } from '@app/core/interfaces';
+import { environment } from '@app/env';
+
+@Pipe({
+	name: 'safeImage',
+	standalone: true,
+})
+export class SafeImagePipe implements PipeTransform {
+	#domSanitizer = inject(DomSanitizer);
+
+	transform(value: Multimedia[]) {
+		if (!value || !value.length) {
+			return '';
+		}
+		const imageUrl =
+			value.find(media => media.subtype === 'master315')?.url ?? value[0].url;
+		return this.#domSanitizer.sanitize(
+			SecurityContext.URL,
+			`${environment.NEW_YORK_TIMES_DOMAIN}${imageUrl}`
+		);
+	}
+}
