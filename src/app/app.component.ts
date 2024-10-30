edd1870f6e5abeb7bb540f31d05c099e66bd2332
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2024-10-30 19:14:39 -0300

    feat: setup app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '@app/core/components';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, NavbarComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
-export class AppComponent {
-	title = 'paycorr-project';
-}
+export class AppComponent {}
