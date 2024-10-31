f561a836b99355209dd7848ad666ab5ff3b3549e
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2024-10-30 23:40:32 -0300

    chore: run lint & format commands

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,11 +1,17 @@
-import { inject, Injector, runInInjectionContext } from '@angular/core';
+import { Injector, inject, runInInjectionContext } from '@angular/core';
 import { Doc, QueryParams, RootApiResponse } from '@app/core/interfaces';
 import { ApiService, NotiflixService } from '@app/core/services';
 import { environment } from '@app/env';
-import { patchState, signalStore, withHooks, withMethods, withState, } from '@ngrx/signals';
+import {
+	patchState,
+	signalStore,
+	withHooks,
+	withMethods,
+	withState,
+} from '@ngrx/signals';
 import { setEntities, withEntities } from '@ngrx/signals/entities';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
-import { fromEvent, Subject, takeUntil, tap } from 'rxjs';
+import { Subject, fromEvent, takeUntil, tap } from 'rxjs';
 
 export type AppState = {
 	isFetching: boolean;
